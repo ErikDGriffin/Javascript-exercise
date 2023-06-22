1b8ac3896879bd679dafb479e6ea826ad09812cd
erik Griffin <griffin.erikd@gmail.com>
2023-06-22 17:00:26 -0400

    updated exercise

---
+++ app.js
@@ -0,0 +1,18 @@
+let LastName= "Griffin";
+var age= 47;
+const firstName= "Erik D";
+var language= "javaScript";
+var createdYear= 1995;
+var isCaseSensitive= "true";
+let price= 19.99;
+let isOnSale= "false";
+let salePercentage= 15;
+let stock= 0;
+let inStock= "false";
+let selectedSize= "M";
+var title= "name of the wind";
+var author= "Patrick Rothfuss";
+var pageCount= 722;
+var bookMark= 456;
+var hasRead= "true";
+
